9b896c645f98ed12d2ca9432cfd2e66131175dbc
Artem Zinoviev <arzinoviev@gmail.com>
2018-06-28 16:04:12 +0300

    move parameters; add debug check

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -12,12 +12,14 @@ class MarkdownHelper
     private $cache;
     private $markdown;
     private $logger;
+    private $isDebug;
 
-    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $markdownLogger)
+    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $markdownLogger, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
+        $this->isDebug = $isDebug;
     }
 
     /**
@@ -29,6 +31,10 @@ class MarkdownHelper
     {
         if (stripos($src, 'beacon') !== false) {
             $this->logger->info('They are talking about beacon again!');
+        }
+
+        if ($this->isDebug) {
+            return $this->markdown->transform($src);
         }
 
         $item = $this->cache->getItem('markdown_'.md5($src));
