e49f3e6fb7397eb6118680adaaa986e545f9e54d
Artem Zinoviev <arzinoviev@gmail.com>
2018-06-28 15:00:40 +0300

    add markdown logger channel

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -4,17 +4,20 @@ namespace App\Service;
 
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
     private $cache;
     private $markdown;
+    private $logger;
 
-    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache)
+    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $logger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
     /**
@@ -24,6 +27,10 @@ class MarkdownHelper
      */
     public function parse(string $src): string
     {
+        if (stripos($src, 'beacon') !== false) {
+            $this->logger->info('They are talking about beacon again!');
+        }
+
         $item = $this->cache->getItem('markdown_'.md5($src));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($src));
