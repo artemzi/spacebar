8b288a5095a62774a1e1b4296f124191f72a1a30
Artem Zinoviev <arzinoviev@gmail.com>
2018-07-03 11:31:49 +0300

    add model fields; refactor templates

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -46,6 +46,10 @@ EOF
             $article->setPublishedAt(new \DateTime(sprintf('-%d days', random_int(1, 100))));
         }
 
+        $article->setAuthor('Mike Doe')
+            ->setHeartCount(random_int(5, 100))
+            ->setImageFilename('asteroid.jpeg');
+
         $em->persist($article);
         $em->flush();
 
