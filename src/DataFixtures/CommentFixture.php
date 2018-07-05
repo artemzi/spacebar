66ed561725d2813fc0873f9153049cd014e98830
Artem Zinoviev <arzinoviev@gmail.com>
2018-07-05 12:59:27 +0300

    refactor fixtures

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -10,13 +10,14 @@ class CommentFixture extends BaseFixtures
 {
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(Comment::class, 100, function(Comment $comment) {
+        $this->createMany(
+            Comment::class, 100, function(Comment $comment) {
             $comment->setContent(
                 $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true)
             );
             $comment->setAuthorName($this->faker->name);
             $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
-            $comment->setArticle($this->getReference(Article::class.'_'.$this->faker->numberBetween(0, 9)));
+            $comment->setArticle($this->getRandomReference(Article::class));
         });
 
         $manager->flush();
