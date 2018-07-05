19ff791dc4257297510f1884a1047e427b3ff3c8
Artem Zinoviev <arzinoviev@gmail.com>
2018-07-05 13:02:01 +0300

    fix class load ordering

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -4,9 +4,10 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class CommentFixture extends BaseFixtures
+class CommentFixture extends BaseFixtures implements DependentFixtureInterface
 {
     protected function loadData(ObjectManager $manager)
     {
@@ -21,5 +22,18 @@ class CommentFixture extends BaseFixtures
         });
 
         $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [
+            ArticleFixtures::class
+        ];
     }
 }
